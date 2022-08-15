4ad32e2ffcf5bd4985460bca3aabf1d463246f97
Shadow Looters <108761193+SVerseLab@users.noreply.github.com>
2022-08-15 20:25:17 +0100

    Remove Liquidity

--- src/views/RemoveLiquidity/index.tsx
+++ src/views/RemoveLiquidity/index.tsx
@@ -168,7 +168,7 @@ export default function RemoveLiquidity() {
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'Shadow LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
