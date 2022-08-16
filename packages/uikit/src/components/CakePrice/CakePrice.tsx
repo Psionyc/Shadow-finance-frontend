88fa59ba7f390423e2aa7e083db7be8c3cff743f
Shadow Looters <108761193+SVerseLab@users.noreply.github.com>
2022-08-16 15:43:45 +0100

    home image

--- packages/uikit/src/components/CakePrice/CakePrice.tsx
+++ packages/uikit/src/components/CakePrice/CakePrice.tsx
@@ -27,7 +27,7 @@ const PriceLink = styled.a`
 const CakePrice: React.FC<Props> = ({ cakePriceUsd, color = "textSubtle", showSkeleton = true }) => {
   return cakePriceUsd ? (
     <PriceLink
-      href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+      href="http://localhost:3000/swap?outputCurrency=0xa48496B0a392007eA9E0354cc804D98444Ecc227"
       target="_blank"
     >
       <LogoRound width="24px" mr="8px" />
